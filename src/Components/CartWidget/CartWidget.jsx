6d64cb1fce2cb2c69d83078304985275b843ffca
Diego Ignacio Cid <diegoigcid@gmail.com>
2022-05-19 12:57:17 -0300

    badge del carrito funcional

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from 'react'
 import { BiCart } from 'react-icons/bi';
 import { BsFillCartCheckFill } from 'react-icons/bs';
 import { GlobalContext } from '../GlobalStateContext/CartContext';
+import "./CartWidget.css"
 
 const Cart = () => {
 
-  const {cart} = useContext(GlobalContext);
+  const {cart, cantidadTotal} = useContext(GlobalContext);
   console.log("Cart en cartwidget: ", cart);
   console.log("Cart length en cartwidget: ", cart.length);
 
@@ -13,7 +14,9 @@ const Cart = () => {
 
   return (
 <>
-    {largo > 0 ? <BsFillCartCheckFill /> : <BiCart />}
+    {/* {largo > 0 ? <BsFillCartCheckFill /> : <BiCart />} */}
+    {largo > 0 ? <i class="fa badge fa-lg" value={cantidadTotal}>&#xf07a;</i> : <></>}
+    
 
     
 </>
