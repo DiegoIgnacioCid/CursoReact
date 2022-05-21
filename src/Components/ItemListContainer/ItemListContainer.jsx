de73c912bf31d1c778de75e74c9154e9e2c9c1b4
Diego Ignacio Cid <diegoigcid@gmail.com>
2022-05-21 10:23:46 -0300

    con Firebase en proceso

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -2,27 +2,72 @@ import React, { useState, useEffect } from 'react'
 import {productos} from "../../Assets/Data/Data"
 import ItemList from '../ItemList/ItemList';
 import "./ItemListContainer.css"
+import db from '../../Service/firebase';
+import { doc, getDoc, getDocs, collection } from "firebase/firestore";
+
+/* OJO PARA TRAER TODOS LOS DOCUMENTOS ES GET DOCS PARA TRAER UNO SOLO GET DOC */
 
 
 const ItemListContainer = () => {
 
     const [dataProds, setdataProds] = useState([]);
 
+    const getData = async () => {
+      const col = collection(db, 'Productos')
+      try {
+        const data = await getDocs(col)
+        const result = data.docs.map(doc => doc = {id: doc.id, ...doc.data()})
+        setdataProds(result)
+        console.log(result)
+      } catch (error) {
+        
+      }
+    }
+
 
     useEffect(() => {
+
+      getData();
+
+
+
+
+     /*  const item = doc(db, 'Productos', '2nvLAsIy3AFogDWH6NkW');
+
+      try {
+        
+        getDoc(item).then((snapshot) => {
+          if(snapshot.exists){
+            setdataProds([...dataProds, {id: snapshot.id, ...snapshot.data()}])
+            console.log(dataProds);
+
+          }
+        })
+        
+      } catch (error) {
+        console.log('Error: ', error)
+      } */
       
-     const myPromise = new Promise(function(myResolve, myReject) {
+
+/* 
+EJEMPLO QUE VIENE DE LOS DOCS DE FIREBASE
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      }); */
+
+
+
+      // uso de la promesa con data local comentado para pasar a Firebase
+     /* const myPromise = new Promise(function(myResolve, myReject) {
         setTimeout(function() { myResolve(productos); }, 2000);
         });
 
         myPromise.then(function(value) {
     
-        /* Aca lo que hacemos con la promesa */
         setdataProds(value);
-        /* console.log(value) */
     })
-    .catch((err) => console.log('Error: ' + err))
-    /* .then(() => console.log(value)); */
+    .catch((err) => console.log('Error: ' + err)) */
       
     }, [])
     
